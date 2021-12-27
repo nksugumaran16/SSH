9d2ab88cd10d5c0794cef497332735b04da307d7
nksugumaran16 <55332000+nksugumaran16@users.noreply.github.com>
2021-12-27 11:24:19 -0600

    Update build.sh

--- Auto/build.sh
+++ Auto/build.sh
@@ -10,9 +10,9 @@ mkdir $version
 #sudo openapi-generator-cli version-manager set 5.3.0
 #sudo npm install @openapitools/openapi-generator-cli -D
 
-cp openapi.yaml $version/
+cp swagger.yaml $version/
 
-openapi-generator-cli generate -g spring -i $version/openapi.yaml -c config.json -o spring-boot-codegenerator
+openapi-generator-cli generate -g spring -i $version/swagger.yaml -c config.json -o spring-boot-codegenerator
 
 cd spring-boot-codegenerator
 
@@ -21,4 +21,4 @@ mvn clean test compile package
 
 #rl -vSf -u "admin:password" -X PUT -T target/spring-boot-codegenerator-1.0.0.jar 'http://192.168.1.67:8082/artifactory/artifactsjar/spring-boot-codegenerator-1.0.0.jar'
 
-curl -vSf -u "admin:password" -X PUT -T "target/spring-boot-codegenerator-$version.jar" "http://192.168.1.67:8082/artifactory/artifactsjar/spring-boot-codegenerator-$version.jar"
+curl -vSf -u "admin:password" -X PUT -T "target/spring-boot-codegenerator-$version.jar" "http://localhost:8082/artifactory/artifactsjar-generic-local//spring-boot-codegenerator-$version.jar"
